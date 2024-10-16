6e0e61b0c1f129a2177f81f660e0ec2631b328bd
rori-eva <erodarivas@gmail.com>
2024-10-16 16:48:08 +0200

    Implementation MVC

--- practica-1-EditableBufferedReader/Version-2-mvc/Console.java
+++ practica-1-EditableBufferedReader/Version-2-mvc/Console.java
@@ -2,11 +2,12 @@
  * Siguiendo el patrón MVC esta clase es del tipo View
  */
 
+import java.io.IOException;
 import java.util.Observable;
 import java.util.Observer;
 
 @SuppressWarnings("deprecation")
-public class Console implements Observer {
+public class Console implements Observer, InterfaceConstantes {
 
     /**
      * Actualiza el objeto observado cada vez que cambia de valor
@@ -16,7 +17,32 @@ public class Console implements Observer {
      */
     @Override
     public void update(Observable o, Object arg) {
-        Line line = (Line) arg;
-        System.out.println("Texto actualizado: " + line.toString());
+        if (o instanceof Line) {
+            Line line = (Line) o;
+            line.printLinea();
+        }
     }
+
+    /**
+     * Configura la consola en modo raw, permitiendo la lectura de teclas especiales, y carácteres
+     * individuales sin buffering
+     * Lee carácter a carácter sin esperar el ENTER
+     * 
+     * @throws  IOException
+
+     */
+    public void setRaw() throws IOException {
+        Runtime.getRuntime().exec(new String[] { "/bin/sh", "-c", "stty -echo raw < /dev/tty" });
+    }
+
+    /**
+     * Configura la consola en modo cooked, permitiendo la lectura de caracteres
+     * Los carácteres son leídos después del ENTER
+     * 
+     * @throws  IOException
+     */
+    public void unsetRaw() throws IOException {
+        Runtime.getRuntime().exec(new String[] { "/bin/sh", "-c", "stty echo cooked < /dev/tty" });
+    }
+
 }
